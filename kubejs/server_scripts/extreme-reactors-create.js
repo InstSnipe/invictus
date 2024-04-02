e8faf1f116a049a17be278f2b06427986b16a5ac
Bt111488 <119612019+Bt111488@users.noreply.github.com>
2024-04-02 14:14:47 -0400

    Add Reactor Casing Recipe And Clean Up Script File

--- kubejs/server_scripts/extreme-reactors-create.js
+++ kubejs/server_scripts/extreme-reactors-create.js
@@ -1,18 +1,23 @@
 ServerEvents.recipes(event => {
+  //Removes all default recipes for extreme reactors
   event.remove({ mod: 'bigreactors' })
+  
+  //Adds custom recipe for verderium
 	event.recipes.createMixing('bigreactors:verderium,2000', [
         '#forge:ingots/uranium',
         '#forge:ingots/uranium',
         'bigreactors:blutonium_ingot'
-      ]).superheated()
-
-      event.recipes.createMixing('bigreactors:butonium,', [
+  ]).superheated()
+  
+  //Adds custom recipe for blutonium
+  event.recipes.createMixing('bigreactors:butonium,', [
         'bigreactors:cyanite_ingot',
         'bigreactors:cyanite_ingot',
         Fluid.of('minecraft:water', 1000)
-      ]).heated()
-
-      event.shaped('bigreactors:reinforced_reactorcontroller', [
+  ]).heated()
+  
+  //Adds custom recipe for reactor controller
+  event.shaped('bigreactors:reinforced_reactorcontroller', [
         'ABA',
         'CDC',
         'AEA'
@@ -22,5 +27,15 @@ ServerEvents.recipes(event => {
         C: 'create_dd:infernal_mechanism',
         D: 'minecraft:diamond_block',
         E: 'minecraft:redstone'
-      })
+  })
+
+  //Adds recipe for reactor casing
+  event.shaped('bigreactors:reinforced_reactorcontroller', [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: '#forge:concrete',
+    B: '#forge:ingots/steel'
+  })
 })
