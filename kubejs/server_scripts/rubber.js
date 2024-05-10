3d0a585b4a4a403490d583a6c1c2bade2b7393ae
SlimeyAr <lostbhijaya@gmail.com>
2024-05-10 16:55:32 +0700

    Add stripping capability

--- kubejs/server_scripts/rubber.js
+++ kubejs/server_scripts/rubber.js
@@ -1,3 +1,10 @@
 onEvent('block.right_click', event => {
-    if (event.block.id == kubejs + 'rubber_log' && event.item.tag) 
+    // let mineableByAxe = new ResourceLoaction('mineable')
+    if (event.block.id == kubejs + 'rubber_log' && event.item.hasTag(minecraft + 'axes'))  {
+        event.block.set(kubejs + 'rubber_stripped_log')
+    }
+    
+    if (event.block.id == kubejs + 'rubber_wood' && event.item.hasTag(minecraft + 'axes'))  {
+        event.block.set(kubejs + 'rubber_stripped_wood')
+    }
 })
