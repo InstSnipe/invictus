6e907782c6b0125debf3cd0df98a402cbbf11459
SlimeyAr <lostbhijaya@gmail.com>
2024-05-10 16:56:46 +0700

    Add way to expose rubber log for tapping purpose

--- kubejs/server_scripts/rubber.js
+++ kubejs/server_scripts/rubber.js
@@ -7,4 +7,8 @@ onEvent('block.right_click', event => {
     if (event.block.id == kubejs + 'rubber_wood' && event.item.hasTag(minecraft + 'axes'))  {
         event.block.set(kubejs + 'rubber_stripped_wood')
     }
+    
+    if (event.block.id == kubejs + 'rubber_log' && event.item.hasTag(minecraft + 'pickaxes'))  {
+        event.block.set(kubejs + 'exposed_rubber_log')
+    }
 })
